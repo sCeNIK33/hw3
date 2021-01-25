1ff91d02a2c638fad8510e932b43dd1007530f7b
Christian Nikolopoulos <cdnikolopoulos@gmail.com>
2021-01-24 21:26:07 -0600

    neater code

--- noober.js
+++ noober.js
@@ -38,46 +38,66 @@ function pageLoad() {
   console.log(levelOfService)
 
   passenger1Name = passenger1.passengerDetails.first + ' ' + passenger1.passengerDetails.last
-  passenger1Phone = passenger1.passengerDetails.phoneNumber
-  passenger1NumberOfPassengers = passenger1.numberOfPassengers
-  passenger1PickupAddressLine1 = passenger1.pickupLocation.address
-  passenger1PickupAddressLine2 = passenger1.pickupLocation.city + ', ' + passenger1.pickupLocation.state + ' ' + passenger1.pickupLocation.zip
-  passenger1DropoffAddressLine1 = passenger1.dropoffLocation.address
-  passenger1DropoffAddressLine2 = passenger1.dropoffLocation.city + ', ' + passenger1.dropoffLocation.state + ' ' + passenger1.dropoffLocation.zip
-  passenger2Name = passenger2.passengerDetails.first + ' ' + passenger2.passengerDetails.last
-  passenger2Phone = passenger2.passengerDetails.phoneNumber
-  passenger2NumberOfPassengers = passenger2.numberOfPassengers
-  passenger2PickupAddressLine1 = passenger2.pickupLocation.address
-  passenger2PickupAddressLine2 = passenger2.pickupLocation.city + ', ' + passenger2.pickupLocation.state + ' ' + passenger2.pickupLocation.zip
-  passenger2DropoffAddressLine1 = passenger2.dropoffLocation.address
-  passenger2DropoffAddressLine2 = passenger2.dropoffLocation.city + ', ' + passenger2.dropoffLocation.state + ' ' + passenger2.dropoffLocation.zip
-  passenger3Name = passenger3.passengerDetails.first + ' ' + passenger3.passengerDetails.last
-  passenger3Phone = passenger3.passengerDetails.phoneNumber
-  passenger3NumberOfPassengers = passenger3.numberOfPassengers
-  passenger3PickupAddressLine1 = passenger3.pickupLocation.address
-  passenger3PickupAddressLine2 = passenger3.pickupLocation.city + ', ' + passenger3.pickupLocation.state + ' ' + passenger3.pickupLocation.zip
-  passenger3DropoffAddressLine1 = passenger3.dropoffLocation.address
-  passenger3DropoffAddressLine2 = passenger3.dropoffLocation.city + ', ' + passenger3.dropoffLocation.state + ' ' + passenger3.dropoffLocation.zip
   console.log(passenger1Name)
+  
+  passenger1Phone = passenger1.passengerDetails.phoneNumber
   console.log(passenger1Phone)
+  
+  passenger1NumberOfPassengers = passenger1.numberOfPassengers
   console.log(passenger1NumberOfPassengers)
+  
+  passenger1PickupAddressLine1 = passenger1.pickupLocation.address
   console.log(passenger1PickupAddressLine1)
+  
+  passenger1PickupAddressLine2 = passenger1.pickupLocation.city + ', ' + passenger1.pickupLocation.state + ' ' + passenger1.pickupLocation.zip
   console.log(passenger1PickupAddressLine2)
+  
+  passenger1DropoffAddressLine1 = passenger1.dropoffLocation.address
   console.log(passenger1DropoffAddressLine1)
+  
+  passenger1DropoffAddressLine2 = passenger1.dropoffLocation.city + ', ' + passenger1.dropoffLocation.state + ' ' + passenger1.dropoffLocation.zip
   console.log(passenger1DropoffAddressLine2)
+  
+  passenger2Name = passenger2.passengerDetails.first + ' ' + passenger2.passengerDetails.last
   console.log(passenger2Name)
+ 
+  passenger2Phone = passenger2.passengerDetails.phoneNumber
   console.log(passenger2Phone)
+  
+  passenger2NumberOfPassengers = passenger2.numberOfPassengers
   console.log(passenger2NumberOfPassengers)
+  
+  passenger2PickupAddressLine1 = passenger2.pickupLocation.address
   console.log(passenger2PickupAddressLine1)
+ 
+  passenger2PickupAddressLine2 = passenger2.pickupLocation.city + ', ' + passenger2.pickupLocation.state + ' ' + passenger2.pickupLocation.zip
   console.log(passenger2PickupAddressLine2)
+  
+  passenger2DropoffAddressLine1 = passenger2.dropoffLocation.address
   console.log(passenger2DropoffAddressLine1)
+
+  passenger2DropoffAddressLine2 = passenger2.dropoffLocation.city + ', ' + passenger2.dropoffLocation.state + ' ' + passenger2.dropoffLocation.zip
   console.log(passenger2DropoffAddressLine2)
+
+  passenger3Name = passenger3.passengerDetails.first + ' ' + passenger3.passengerDetails.last
   console.log(passenger3Name)
+
+  passenger3Phone = passenger3.passengerDetails.phoneNumber
   console.log(passenger3Phone)
+
+  passenger3NumberOfPassengers = passenger3.numberOfPassengers
   console.log(passenger3NumberOfPassengers)
+
+  passenger3PickupAddressLine1 = passenger3.pickupLocation.address  
   console.log(passenger3PickupAddressLine1)
+ 
+  passenger3PickupAddressLine2 = passenger3.pickupLocation.city + ', ' + passenger3.pickupLocation.state + ' ' + passenger3.pickupLocation.zip
   console.log(passenger3PickupAddressLine2)
+
+  passenger3DropoffAddressLine1 = passenger3.dropoffLocation.address
   console.log(passenger3DropoffAddressLine1)
+
+  passenger3DropoffAddressLine2 = passenger3.dropoffLocation.city + ', ' + passenger3.dropoffLocation.state + ' ' + passenger3.dropoffLocation.zip
   console.log(passenger3DropoffAddressLine2)
 
 
