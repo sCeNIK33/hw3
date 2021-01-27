d9a7e2634621e7540b396ba4c8c5144576f2a787
Christian Nikolopoulos <cdnikolopoulos@gmail.com>
2021-01-26 19:12:21 -0600

    experimenting since data seems false

--- noober.js
+++ noober.js
@@ -28,13 +28,22 @@ function pageLoad() {
   let passenger3 = ride[2]
   
   levelOfService = ('Noober X', 'Noober Purple', 'Noober XL', 'Noober Pool')
-  if (ride.length > 1 && passenger1.numberOfPassengers == 1) {
+  if (ride.length > 1) {
     levelOfService = 'Noober Pool'
-  } else if (passenger1.purpleRequested = true && passenger1.numberOfPassengers <= 3) {
+  } else if (ride.purpleRequested = true && passenger1.numberOfPassengers <= 3) {
     levelOfService = 'Noober Purple'
-  } else if (passenger1.numberOfPassengers > 3) {
+  } else if (ride.numberOfPassengers>3) {
     levelOfService = 'Noober XL'
   } else {levelOfService = 'Noober X'}
+  
+  //levelOfService = ('Noober X', 'Noober Purple', 'Noober XL', 'Noober Pool')
+  //if (ride.length > 1 && passenger1.numberOfPassengers == 1) {
+  //  levelOfService = 'Noober Pool'
+  //} else if (passenger1.purpleRequested = true && passenger1.numberOfPassengers <= 3) {
+  //  levelOfService = 'Noober Purple'
+  //} else if (passenger1.numberOfPassengers > 3) {
+  //  levelOfService = 'Noober XL'
+  //} else {levelOfService = 'Noober X'}
   
   console.log(levelOfService)
 
